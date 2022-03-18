0655ea13f00f478172e3fe47d6122798236fa578
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-03-18 21:29:07 +0300

    ДЗ для БАРС Груп

--- For_Bars/Program.cs
+++ For_Bars/Program.cs
@@ -1,33 +1,21 @@
 ﻿using System;
 var worker = new Test();
-worker.OnError += LogError;
 worker.OnKeyPressed += (o,name) => Console.WriteLine($"Введённый символ {name}");;
 worker.Run();
-static void LogError(object sender, Exception e) {
-    Console.WriteLine(e.Message); 
-}
         
 public class Test
 {
-    public event EventHandler<Exception> OnError;
     public event EventHandler<char> OnKeyPressed;
     public void Run()
     {
-       try
+       while (true)
        {
-            while (true)
-            {
-                char name = char.Parse(Console.ReadLine());
-                OnKeyPressed?.Invoke(this, name);
-                if (name == 'c') break; //Английская
-                if (name == 'с') break; //Русская
-            }
+            char name = char.Parse(Console.ReadLine());
+            OnKeyPressed?.Invoke(this, name);
+            if (name == 'c') break; //Английская
+            if (name == 'с') break; //Русская
        }
-       catch(Exception ex)
-       {
-            OnError?.Invoke(this, ex);
-       }
-        }
     }
+}
     
 
