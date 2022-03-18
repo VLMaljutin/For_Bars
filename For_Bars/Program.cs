88a974e84f73e508ba60b684b1ec77b6464895b1
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-03-18 12:40:35 +0300

    Добавьте файлы проекта.

---
+++ For_Bars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_Bars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
