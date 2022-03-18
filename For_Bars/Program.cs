c7628cce5c088684bf0c3278748d356b800d3e85
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-03-18 21:26:24 +0300

    ДЗ БАРС Груп

--- For_Bars/Program.cs
+++ For_Bars/Program.cs
@@ -1,31 +1,33 @@
 ﻿using System;
-
-namespace For_Bars
+var worker = new Test();
+worker.OnError += LogError;
+worker.OnKeyPressed += (o,name) => Console.WriteLine($"Введённый символ {name}");;
+worker.Run();
+static void LogError(object sender, Exception e) {
+    Console.WriteLine(e.Message); 
+}
+        
+public class Test
 {
-    class Program
+    public event EventHandler<Exception> OnError;
+    public event EventHandler<char> OnKeyPressed;
+    public void Run()
     {
-        static void Main(string[] args)
-        {
-            var worker = new Test();
-            worker.OnKeyPressed += (o,name) => Console.WriteLine($"Введённый символ {name}");;
-            worker.Run();
-        }
-
-    }
-
-    public class Test
-    {
-        public event EventHandler<char> OnKeyPressed;
-        public void Run()
-        {
+       try
+       {
             while (true)
             {
                 char name = char.Parse(Console.ReadLine());
                 OnKeyPressed?.Invoke(this, name);
-                if (name == 'c' ) break; //Английская
-                if (name == 'с' ) break; //Русская
+                if (name == 'c') break; //Английская
+                if (name == 'с') break; //Русская
             }
+       }
+       catch(Exception ex)
+       {
+            OnError?.Invoke(this, ex);
+       }
         }
     }
     
-}
+
