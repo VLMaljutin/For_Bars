e64d91c9f76db2197c83df43e189a52cd2ad9146
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-03-25 20:00:46 +0300

    Обобщения

--- ForBars_2/Program.cs
+++ ForBars_2/Program.cs
@@ -1,12 +1,40 @@
 ﻿// Задание 1 - обобщение 
-Exception ex = new Exception();
 // Тип int
 LocalFileLogger<int> x = new LocalFileLogger<int>("C:/Users/Vasiliy/source/repos/For_Bars/ForBars_2/Test.txt");
+DivideByZeroException ex_int = new DivideByZeroException();
 x.LogInfo("Var Int");
-x.LogWarning("Var Int");
-x.LogError("Var Int", ex);
+x.LogWarning("Warnig Int");
+x.LogError("Error Int", ex_int);
 
-//Тип 
+//Тип string
+LocalFileLogger<string> str = new LocalFileLogger<string>("C:/Users/Vasiliy/source/repos/For_Bars/ForBars_2/Test.txt");
+FormatException ex_str = new FormatException();
+str.LogInfo("Var String");
+str.LogWarning("Warning String");
+str.LogError("Error String", ex_str);
+
+//Тип bool
+LocalFileLogger<bool> var_bool = new LocalFileLogger<bool>("C:/Users/Vasiliy/source/repos/For_Bars/ForBars_2/Test.txt");
+NotSupportedException ex_bool = new NotSupportedException();
+var_bool.LogInfo("Var Bool");
+var_bool.LogWarning("Warning Bool");
+var_bool.LogError("Error Bool", ex_bool);
+
+//Тип float
+LocalFileLogger<float> var_float = new LocalFileLogger<float>("C:/Users/Vasiliy/source/repos/For_Bars/ForBars_2/Test.txt");
+ArgumentException ex_float = new ArgumentException();
+var_float.LogInfo("Var Float");
+var_float.LogWarning("Warning Float");
+var_float.LogError("Error Float", ex_float);
+
+//Тип object
+LocalFileLogger<object> var_object = new LocalFileLogger<object>("C:/Users/Vasiliy/source/repos/For_Bars/ForBars_2/Test.txt");
+TimeoutException ex_object = new TimeoutException();
+var_object.LogInfo("Var Object");
+var_object.LogWarning("Warning Object");
+var_object.LogError("Error Object", ex_object);
+
+
 
 public interface ILogger{
     public void LogInfo(string message);
