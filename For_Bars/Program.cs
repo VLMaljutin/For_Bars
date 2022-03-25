01dd528e4e49efaa7ece63216a6df8d09734c15f
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-03-25 20:15:47 +0300

    Update All

--- For_Bars/Program.cs
+++ For_Bars/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 var worker = new Test();
-worker.OnKeyPressed += (o,name) => Console.WriteLine($"Введённый символ {name}");;
+worker.OnKeyPressed += (o, name) => Console.WriteLine($"Введённый символ {name}"); ;
 worker.Run();
-        
+
 public class Test
 {
     public event EventHandler<char> OnKeyPressed;
     public void Run()
     {
-       while (true)
-       {
+        while (true)
+        {
             char name = char.Parse(Console.ReadLine());
             OnKeyPressed?.Invoke(this, name);
             if (name == 'c') break; //Английская
             if (name == 'с') break; //Русская
-       }
+        }
     }
 }
-    
-
